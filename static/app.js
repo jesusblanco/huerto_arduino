e1580f9780b08d9cadea4b916ae2c6204ca0657a
JuanPablo <jpabloaj@gmail.com>
2016-03-11 20:49:32 -0300

    agregados graficos de humedad y suelo

--- static/app.js
+++ static/app.js
@@ -1,5 +1,5 @@
 $(function () {
-  var options = {
+  var optionsTemp = {
     title:{
       text: "Huerto Arduino"
     },
@@ -9,20 +9,63 @@ $(function () {
       }
     },
     chart: {
-      renderTo: 'container'
+      renderTo: 'container-temperatura'
+    },
+    series: [{}]
+  };
+
+  var optionsHum = {
+    title:{
+      text: ""
+    },
+    yAxis:{
+      title: {
+        text:'Humedad (%)'
+      }
+    },
+    chart: {
+      renderTo: 'container-humedad'
+    },
+    series: [{}]
+  };
+
+  var optionsSuelo = {
+    title:{
+      text: ""
+    },
+    yAxis:{
+      title: {
+        text:'Humedad Suelo'
+      }
+    },
+    chart: {
+      renderTo: 'container-suelo'
     },
     series: [{}]
   };
   $.getJSON('/data', function(data){
 
-    var arr = [];
+    var arrTemp = [];
+    var arrHum = [];
+    var arrSuelo = [];
 
     for (var i = 0; i < data.measures.length; i++) {
-      arr.push([data.measures[i].fecha, data.measures[i].temperatura]);
+      arrTemp.push([data.measures[i].fecha, data.measures[i].temperatura]);
+      arrHum.push([data.measures[i].fecha, data.measures[i].humedad]);
+      arrSuelo.push([data.measures[i].fecha, data.measures[i].h_suelo]);
     }
 
-    options.series[0].data = arr;
-    options.series[0].name = 'Temperatura';
-    var chart = new Highcharts.Chart(options)
+    optionsTemp.series[0].data = arrTemp;
+    optionsTemp.series[0].name = 'Temperatura';
+    var chartTemp = new Highcharts.Chart(optionsTemp);
+
+    optionsHum.series[0].data = arrHum;
+    optionsHum.series[0].name = 'Humedad';
+    var chartHum = new Highcharts.Chart(optionsHum);
+
+    optionsSuelo.series[0].data = arrSuelo;
+    optionsSuelo.series[0].name = 'Humedad Suelo';
+    var chartSuelo = new Highcharts.Chart(optionsSuelo);
+
   });
 });
